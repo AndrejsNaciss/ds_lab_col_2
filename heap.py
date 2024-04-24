c1e65d95118a8c05a840bd2ec49624f31af1105b
AndrejsNaciss <143951491+AndrejsNaciss@users.noreply.github.com>
2024-04-24 15:54:53 +0300

    24.04

--- heap.py
+++ heap.py
@@ -57,28 +57,28 @@ class MaxHeap:
 
 
 def find_kth_smallest(nums, k):
-    ###################################
-    ###                             ###
-    ###                             ###
-    ###                             ###
-    ###################################
-
-
-
+    max_heap = MaxHeap()  # maksimālā kaudze
+    for num in nums:
+        max_heap.insert(num)
+        
+        if len(max_heap.heap) > k:
+            max_heap.remove()
+                
+    return max_heap.heap[0]
 
 # Test cases
-nums = [[3,2,1,5,6,4], [6,5,4,3,2,1], [1,2,3,4,5,6], [3,2,3,1,2,4,5,5,6]]
-ks = [2, 3, 4, 7]
-expected_outputs = [2, 3, 4, 5]
+# nums = [[3,2,1,5,6,4], [6,5,4,3,2,1], [1,2,3,4,5,6], [3,2,3,1,2,4,5,5,6]]
+# ks = [2, 3, 4, 7]
+# expected_outputs = [2, 3, 4, 5]
 
-for i in range(len(nums)):
-    print(f'Test case {i+1}...')
-    print(f'Input: {nums[i]} with k = {ks[i]}')
-    result = find_kth_smallest(nums[i], ks[i])
-    print(f'Output: {result}')
-    print(f'Expected output: {expected_outputs[i]}')
-    print(f'Test passed: {result == expected_outputs[i]}')
-    print('---------------------------------------')
+# for i in range(len(nums)):
+#    print(f'Test case {i+1}...')
+#    print(f'Input: {nums[i]} with k = {ks[i]}')
+#    result = find_kth_smallest(nums[i], ks[i])
+#    print(f'Output: {result}')
+#    print(f'Expected output: {expected_outputs[i]}')
+#    print(f'Test passed: {result == expected_outputs[i]}')
+#    print('---------------------------------------')
 
 
 """
@@ -110,4 +110,3 @@ for i in range(len(nums)):
     ---------------------------------------
 
 """
-
